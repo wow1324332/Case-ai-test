1fb8db200b221bb98c3c93a334d0797fcb4306e1
wow1324332 <wow1324332@gmail.com>
2026-05-13 08:38:48 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../App.jsx' // Fix: Update the import path for App
-// Remove the import for index.css as it is not needed for the single-file setup
-// import './index.css' 
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
