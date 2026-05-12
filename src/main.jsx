b6d47a9fa90e3dc11b02ea3b819ff8738bc376c2
wow1324332 <wow1324332@gmail.com>
2026-05-13 08:42:14 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// 반드시 App.jsx가 같은 폴더에 있어야 하고, 점 하나(./)로 시작해야 합니다.
+import App from './App.jsx' 
+// 반드시 index.css가 같은 폴더에 있어야 하고, 점 하나(./)로 시작해야 합니다.
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
